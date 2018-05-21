81d028313957df22b70fb24188ef9e88e6ba8744
Eric Lake <ericlake@gmail.com>
2018-05-21 17:59:10 -0500

    remove math, refactor difference_of_squares

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -1,9 +1,5 @@
 package diffsquares
 
-import (
-	"math"
-)
-
 // SquareOfSums receives a number and takes a range from 1 to
 // number, adds each number in the range then returns the square
 // of the sum.
@@ -12,17 +8,16 @@ func SquareOfSums(number int) int {
 	for i := 1; i <= number; i++ {
 		sum += i
 	}
-	return int(math.Pow(float64(sum), float64(2)))
+	return sum * sum
 }
 
 // SumOfSquares receives a number and takes a range from 1 to
 // number, adds the square of each number in the range then returns
 // the sum.
 func SumOfSquares(number int) int {
-	var sum float64
-	sum = 0
+	sum := 0
 	for i := 1; i <= number; i++ {
-		sum += math.Pow(float64(i), float64(2))
+		sum += i * i
 	}
 	return int(sum)
 }
